b4dda0ae7d9dc463ba2bfd28e17837d9ec97f089
Ephraimiyanda <iyandaephraim@gmail.com>
2023-04-17 13:39:46 -0900

    project finished

--- src/component/nav.jsx
+++ src/component/nav.jsx
@@ -2,24 +2,29 @@ import Btn from './props/button'
 import Logo from "../../images/logo.svg"
 import hamburger from "../../images/icon-hamburger.svg"
 import close from "../../images/icon-close.svg"
+import "../../src/index.css"
+import { useState } from 'react'
 function Nav(){
+
+const [nav, showNavbar]=useState(false)
     return(
 
 <div className="navbar">
-<div className='img-div'>
     <img className='logo1' src={Logo} alt="" />
-<img className='hamburger' src={hamburger} alt="" /></div>
-<div>
-<ul className='link-container'>
-    <a className='nav-links' href="#">Home</a> 
-    <a className='nav-links' href="#">About </a>
-    <a className='nav-links' href="#">Contact</a>
-    <a className='nav-links' href="#">Blog </a>
-    <a className='nav-links' href="#"> Careers </a>
-</ul>
-</div>
-<Btn class="request-btn no-display"/>
 
+<div>
+<ul className={nav ? "display" : "link-container"}>
+        <a className='nav-links' href="#">Home</a> 
+        <a className='nav-links' href="#">About </a>
+        <a className='nav-links' href="#">Contact</a>
+        <a className='nav-links' href="#">Blog </a>
+        <a className='nav-links' href="#"> Careers </a>
+    </ul>
+</div>
+<img className='hamburger' onClick={()=>{
+ showNavbar(!nav)
+}} src={nav ? close:hamburger} alt="" />
+<Btn class="request-btn no-display"/>
 </div>
     )
 }
